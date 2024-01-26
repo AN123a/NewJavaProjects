909f7bbde11f8e07717b08c40760e6fa0182f782
Anjali <anjali265580@gmail.com>
2024-01-26 12:22:18 +0530

    Java Task

---
+++ Anjali/src/tnsiftasks/Pattern8.java
@@ -0,0 +1,26 @@
+package tnsiftasks;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0 ; i<=n ;i++) {
+			for(int j=0;j<=n;j++) {
+				if(j==0  ||  j==n || i==n/2) {
+					System.out.print("*");
+
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+
+		}
+		sc.close();
+
+	}
+
+}
